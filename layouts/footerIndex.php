b4c869b794ddcd74ac9ee35c58908e9c7af52fd4
FisitoDownMadg <139363517+fisitodown012023@users.noreply.github.com>
2023-09-11 11:47:42 -0500

    Add files via upload

--- layouts/footerIndex.php
+++ layouts/footerIndex.php
@@ -31,21 +31,15 @@
             <h2 class="titulo" style="font-weight: bold;">Acerca de:</h2>
             <ul class="integrantes" style="text-align: center;">
                 <li class="listaintegrantes">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-circle" viewBox="0 0 16 16">
-                        <circle cx="8" cy="8" r="7"/>
-                    </svg>
+                <img src="assets/imagenes/team-3-svgrepo-com.svg" alt="equipo" width="40" height="40">
                     <a href="equipo.php">Equipo de desarrolladores</a>
                 </li>
                 <li class="listaintegrantes">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-square" viewBox="0 0 16 16">
-                        <rect width="12" height="12" rx="2"/>
-                    </svg>
+                <img src="assets/imagenes/star-fall-minimalistic-2-svgrepo-com.svg" alt="estrella" width="40" height="40">
                     <a href="mision.php">Misión</a>
                 </li>
                 <li class="listaintegrantes">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-triangle" viewBox="0 0 16 16">
-                        <path d="M7.36.086a1 1 0 0 1 1.28 0l6 5a1 1 0 0 1-.64 1.828H1.72a1 1 0 0 1-.64-1.828z"/>
-                    </svg>
+                <img src="assets/imagenes/mail-svgrepo-com.svg" alt="correo" width="40" height="40">
                     <a href="contactos.php">Contáctanos</a>
                 </li>
             </ul>
