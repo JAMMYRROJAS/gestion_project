92d47ff63cd066209abd7679618d15d10c8b2c2a
FisitoDownMadg <139363517+fisitodown012023@users.noreply.github.com>
2023-09-12 04:05:24 -0500

    cambios en ese logo de miercoles para ver si ahora si funciona

--- layouts/footer.php
+++ layouts/footer.php
@@ -20,35 +20,50 @@
             margin-right: auto;/* Altura del viewport */
         }
     </style>
-    <div style="display: flex; justify-content: space-between">
-        <div class="contenedor" style="margin-left: 30px;">
-            <ul>
-                <li class="logoimg" style="height: 100px; width: 100px;"><a href="https://enlinea.unapiquitos.edu.pe/"><img src="assets/logo/logoUNAP-blanco.png" alt="logoUNAPblanco"></a></li>
-                <li class="logo" style="font-size: 40px; font-weight: bold; margin-top: 50px;">Facultad de Ingeniería de Sistemas e Informática</li>
-            </ul>
+    <div class="container pt-4">
+  <div class="row">
+    <!-- Columna izquierda -->
+    <div class="col-md-6">
+      <div class="d-flex justify-content-center align-items-center flex-column">
+        <div class="logoimg">
+          <a href="https://enlinea.unapiquitos.edu.pe/"><img src="assets/logo/logoUNAP-blanco.png" alt="logoUNAPblanco" style="height: 150px; width: 200;"></a>
         </div>
-        <div class="contenedor" style="margin-right: 30px;">
-            <h2 class="titulo" style="font-weight: bold;">Acerca de:</h2>
-            <ul class="integrantes" style="text-align: center;">
-                <li class="listaintegrantes">
-                <img src="assets/imagenes/team-3-svgrepo-com.svg" alt="equipo" width="40" height="40">
-                    <a href="equipo.php">Equipo de desarrolladores</a>
-                </li>
-                <li class="listaintegrantes">
-                <img src="assets/imagenes/star-fall-minimalistic-2-svgrepo-com.svg" alt="estrella" width="40" height="40">
-                    <a href="mision.php">Misión</a>
-                </li>
-                <li class="listaintegrantes">
-                <img src="assets/imagenes/mail-svgrepo-com.svg" alt="correo" width="40" height="40">
-                    <a href="contactos.php">Contáctanos</a>
-                </li>
-            </ul>
+        <div class="logo" style="font-size: 40px; font-weight: bold; margin-top: 50px; text-align: center;">
+          Facultad de Ingeniería de Sistemas e Informática
         </div>
+      </div>
+    </div>
+    
+    <!-- Columna derecha -->
+    <div class="col-md-6">
+      <div class="d-flex justify-content-center align-items-center flex-column">
+        <h2 class="titulo" style="font-weight: bold; text-align: center;">Acerca de:</h2>
+        <ul class="integrantes">
+          <li class="listaintegrantes">
+            <img src="assets/imagenes/team-3-svgrepo-com.svg" alt="equipo" width="40" height="40">
+            <a href="equipo.php">Equipo</a>
+            <p>Acerca de nosotros...</p> <!-- Agregar descripción aquí -->
+          </li>
+          <li class="listaintegrantes">
+            <img src="assets/imagenes/star-fall-minimalistic-2-svgrepo-com.svg" alt="estrella" width="40" height="40">
+            <a href="mision.php">Misión</a>
+            <p>Metas y objetivos...</p> <!-- Agregar descripción aquí -->
+          </li>
+          <li class="listaintegrantes">
+            <img src="assets/imagenes/mail-svgrepo-com.svg" alt="correo" width="40" height="40">
+            <a href="contactos.php">Contáctanos</a>
+            <p>Nuestras redes sociales...</p> <!-- Agregar descripción aquí -->
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
 
-    </div>
-    <div style="text-align: center; padding-bottom: 30px; background: #f5576c;">
-        <p>© Desarrollado por GPTI-FISI</p>
-    </div>
+<div style="text-align: center; margin-top: 50px; padding-bottom: 30px; background: #f5576c;">
+  <p>© Desarrollado por GPTI-FISI</p>
+</div>
+
     </footer>
 
     <script src="https://kit.fontawesome.com/c15b744a04.js" crossorigin="anonymous"></script>
